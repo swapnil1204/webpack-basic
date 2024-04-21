3ec3ccba9f3b29c1cfb727abf22210267d234721
Swapnil <swapnilbamb1204@gmail.com>
2024-04-21 13:17:08 -0500

    webpack basic config for react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import react from "react";
+import { render } from "react-dom";
+
+const Greeting = () => <h1>Hello from React.</h1>
+
+render(
+    <Greeting/>,
+    document.getElementById('target')
+)
